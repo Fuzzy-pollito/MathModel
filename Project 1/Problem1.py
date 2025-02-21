009c35fb2e56b0d31db8addbe62cbd8d3b5d42b2
mikkelherskindgudmandsen <s214127@dtu.dk>
2025-02-21 14:45:34 +0100

    Problem 1, 2.1, 2.2

--- Project 1/Problem1.py
+++ Project 1/Problem1.py
@@ -13,14 +13,11 @@ image_files = sorted([f for f in os.listdir(folder_path) if f.endswith(".png")])
 images = np.array([iio.imread(os.path.join(folder_path, f), mode="L") / 255.0 for f in image_files])
 
 print(images.shape)  # (num_images, height, width)
-plt.ion()  # Turn on interactive mode'
 
 if __name__ == "__main__":
     plt.figure()
     for frame in images:
-        plt.imshow(frame, cmap="gray")  # Display the current frame
-        plt.axis("off")  # Hide axes
-        plt.pause(0.01)  # Pause for 50ms
+        plt.imshow(frame, cmap="gray")
+        plt.axis("off")
+        plt.pause(0.01)
         plt.clf()  # Clear figure for next frame
-    plt.ioff()  # Turn off interactive mode
-    plt.show()  # Show the final figure
